a74b9252c338da9ca7a3aa7c13f9ca25ca0730e8
Kumani K <kumanikidd@gmail.com>
2023-10-17 20:23:31 +0100

    UIkit barcode scanner view controller complete

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  BarcodeScannerView.swift
 //  BarcodeScanner
 //
 //  Created by Kumani Kidd on 17/10/2023.
@@ -7,18 +7,39 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct BarcodeScannerView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+              
+                
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer()
+                    .frame( maxHeight: 85)
+                
+                HStack{
+                    Label("Scanned Barcode", systemImage: "barcode.viewfinder")
+                        .fontWeight(.light)
+                        .font(.system(size: 20))
+                }
+                .frame(width: 400, height: 50)
+                
+                Text("Not Yet Scanned")
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .foregroundStyle(.red)
+                    .padding()
+                
+               
+            }
+            .navigationTitle("Barcode Scanner")
+            
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    BarcodeScannerView()
 }
